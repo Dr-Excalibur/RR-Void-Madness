2d14513cc616f1249098a773ad88aebad584dd25
Excalibur2289 <jonathanlabreche@Jonathans-MacBook-Air.local>
2016-02-29 12:01:19 -0600

    Second commit

--- Functions.lua
+++ Functions.lua
@@ -1,4 +1,4 @@
-#
+--This is an extremely early alpha version of the function database. It will stay in basic lua form until we decide on ComputerCraft or OpenComputers.
 function "initial"
     print "Welcome to RR:Void!"
     os.sleep(2)
@@ -8,7 +8,10 @@ function "initial"
     os.sleep(2)
     print "Keep in mind that this is intentional"
     os.sleep(2)
-    print "If you understand this, pleas type @apsI Agree@aps"
+    print "If you understand this, please type @apsI Agree@aps"
+    --Some kind of command that waits for the input
+    print "Excellent! Enjoy your maddening adventure."
+    command.
 end
 
 if tutorial==true then
