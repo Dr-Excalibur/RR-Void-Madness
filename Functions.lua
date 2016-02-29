ab673f93750358ff7a2fc411c9b92e278e5079a5
Excalibur2289 <jonathanlabreche@Jonathans-MacBook-Air.local>
2016-02-29 11:31:04 -0600

    Making sure that it works NOW

---
+++ Functions.lua
@@ -0,0 +1,15 @@
+#
+function "initial"
+    print "Welcome to RR:Void!"
+    os.sleep(2)
+    print "This is not like any other skyblock you've played before"
+    os.sleep(2)
+    print "One simple little mistake is all it takes to send everything you've worked hard to build careening into the Void."
+    os.sleep(2)
+    print "Keep in mind that this is intentional"
+    os.sleep(2)
+    print "If you understand this, pleas type @apsI Agree@aps"
+end
+
+if tutorial==true then
+    
