55bff35fe8a8f97f8a175c0af505b8e818c345c2
Excalibur2289 <jonathanlabreche@Jonathans-MacBook-Air.local>
2016-03-01 10:59:45 -0600

    First function test
    Cleaned up some of the shards of code near the bottom of the file.
    Initial function needs to be tested

--- The Machine/Functions.lua
+++ The Machine/Functions.lua
@@ -1,6 +1,7 @@
 --This is an extremely early alpha version of the function database. It will stay in basic lua form until we decide on ComputerCraft or OpenComputers.
---[[
+--
 function "initial"
+    --This function runs the first time someone joins a world, or pushes the button in the spawn room
     say("Welcome to RR:Void!")
     os.sleep(2)
     say("This is not like any other skyblock you've played before")
@@ -9,13 +10,13 @@ function "initial"
     os.sleep(2)
     say("Keep in mind that this is intentional")
     os.sleep(2)
-    say("If you understand this, please type @apsI Agree@aps")
-    --Some kind of command that waits for the input
-    say("Excellent! Enjoy your maddening adventure.)
-    command.
+    say("If you understand this, please type @apsI Agree@aps. Capitalization matters!")
+    local event code = os.pullEvent("chat.message")
+    if code == "I Agree" then
+        say("Excellent! Enjoy your maddening adventure.)
+        command.
+        tutorial = true
+    end
 end
---]]
-if
-
-if tutorial==true then
-    
+        --]]
+initial()
