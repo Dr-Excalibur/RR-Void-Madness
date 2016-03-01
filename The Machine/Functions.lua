69e01128bccc7316155710bb289d7cf61ee36242
Excalibur2289 <jonathanlabreche@Jonathans-MacBook-Air.local>
2016-03-01 09:27:56 -0600

    Updated print commands to Computronics chat box function
    Still need to figure out how to have the computer “read” the chat

--- The Machine/Functions.lua
+++ The Machine/Functions.lua
@@ -1,18 +1,21 @@
 --This is an extremely early alpha version of the function database. It will stay in basic lua form until we decide on ComputerCraft or OpenComputers.
+--[[
 function "initial"
-    print "Welcome to RR:Void!"
+    say("Welcome to RR:Void!")
     os.sleep(2)
-    print "This is not like any other skyblock you've played before"
+    say("This is not like any other skyblock you've played before")
     os.sleep(2)
-    print "One simple little mistake is all it takes to send everything you've worked hard to build careening into the Void."
+    say("One simple little mistake is all it takes to send everything you've worked hard to build careening into the Void.")
     os.sleep(2)
-    print "Keep in mind that this is intentional"
+    say("Keep in mind that this is intentional")
     os.sleep(2)
-    print "If you understand this, please type @apsI Agree@aps"
+    say("If you understand this, please type @apsI Agree@aps")
     --Some kind of command that waits for the input
-    print "Excellent! Enjoy your maddening adventure."
+    say("Excellent! Enjoy your maddening adventure.)
     command.
 end
+--]]
+if
 
 if tutorial==true then
     
